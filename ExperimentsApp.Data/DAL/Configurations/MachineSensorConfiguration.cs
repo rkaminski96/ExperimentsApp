28e2d65f7e4a711708c6c968911bab9c3a7e69fe
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2018-12-14 13:43:36 +0100

    Added DbContex, before first migration

---
+++ ExperimentsApp.Data/DAL/Configurations/MachineSensorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ExperimentsApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace ExperimentsApp.Data.DAL.Configurations
+{ 
+    internal class MachineSensorConfiguration : IEntityTypeConfiguration<MachineSensor>
+    {
+        public void Configure(EntityTypeBuilder<MachineSensor> builder)
+        {
+            builder.HasKey(ms => new { ms.MachineId, ms.SensorId });
+        }
+    }
+}
---
+++ ExperimentsApp.Data/DAL/ExperimentsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExperimentsApp.Data.Model;
+using ExperimentsApp.Data.DAL.Configurations;
+
+
+namespace ExperimentsApp.Data.DAL
+{
+    public class ExperimentsDbContext : DbContext
+    {
+        public ExperimentsDbContext(DbContextOptions<ExperimentsDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Experiment> Experiments { get; set; }
+        public DbSet<ExperimentType> ExperimentTypes { get; set; }
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<MachineSensor> MachineSensors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new MachineSensorConfiguration());
+        }
+
+    }
+}
