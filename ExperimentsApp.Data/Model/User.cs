92673387cd8189b6f37e79bdf9022e44256885b5
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2018-12-28 17:58:04 +0100

    Added migration (create User model) and updated database

--- ExperimentsApp.Data/Model/User.cs
+++ ExperimentsApp.Data/Model/User.cs
@@ -9,7 +9,7 @@ namespace ExperimentsApp.Data.Model
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        public string Username{ get; set; }
         public string PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
