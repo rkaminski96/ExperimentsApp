364002d1c7ce229b5c751e709cf255a8644786c3
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2019-01-21 03:16:22 +0100

    database updated

--- ExperimentsApp.Data/Model/Experiment.cs
+++ ExperimentsApp.Data/Model/Experiment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExperimentsApp.Data.Model
@@ -18,6 +19,8 @@ namespace ExperimentsApp.Data.Model
         [ForeignKey("Machine")]
         public int MachineId { get; set; }
         public Machine Machine { get; set; }
+
+        public IList<ExperimentSensor> ExperimentSensors { get; set; }
     }
 }
  
